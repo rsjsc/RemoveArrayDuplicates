777d370a177122c485d19a03d31e385f3c76affa
Richard Sandberg <richard.sandberg@jsc.se>
2023-06-13 15:06:22 +0200

    Test

--- HttpTrigger1.cs
+++ HttpTrigger1.cs
@@ -2,33 +2,35 @@ using System.Net;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Azure.Functions.Worker.Http;
 using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using Company.Function.StringArraySorter;
 
 namespace Company.Function
 {
     public class HttpTrigger1
+{
+    [Function("HttpTrigger1")]
+    public static HttpResponseData Run(
+        [HttpTrigger(AuthorizationLevel.Function, "post")] HttpRequestData req,
+        FunctionContext executionContext)
     {
-        [Function("HttpTrigger1")]
-        public static HttpResponseData Run(
-            [HttpTrigger(AuthorizationLevel.Function, "post")] HttpRequestData req,
-            FunctionContext executionContext)
-        {
-            var logger = executionContext.GetLogger("HttpTrigger1");
-            logger.LogInformation("C# HTTP trigger function processed a request.");
+        var logger = executionContext.GetLogger("HttpTrigger1");
+        logger.LogInformation("C# HTTP trigger function processed a request.");
 
-            // Läs inkommande data från request body
-            string requestBody = req.ReadString();
+        // Läs inkommande data från request body
+        string requestBody = req.ReadString();
 
-            // Omvandla JSON-data till array av strängar
-            string[] inputArray = Newtonsoft.Json.JsonConvert.DeserializeObject<string[]>(requestBody);
+        // Omvandla JSON-data till array av strängar
+        string[] inputArray = Newtonsoft.Json.JsonConvert.DeserializeObject<string[]>(requestBody);
 
-            // Sortera arrayen
-            string[] sortedArray = StringArraySorter.SortStringArray(inputArray);
+        // Sortera arrayen
+        string[] sortedArray = HttpTrigger1.SortStringArray(inputArray);
 
-            // Returnera den sorterade arrayen som JSON
-            var response = req.CreateResponse(HttpStatusCode.OK);
-            response.WriteString(Newtonsoft.Json.JsonConvert.SerializeObject(sortedArray));
+        // Returnera den sorterade arrayen som JSON
+        var response = req.CreateResponse(HttpStatusCode.OK);
+        response.WriteString(Newtonsoft.Json.JsonConvert.SerializeObject(sortedArray));
 
-            return response;
-        }
+        return response;
     }
+}
 }
