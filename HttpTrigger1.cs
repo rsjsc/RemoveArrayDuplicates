8caa384cd62cf30aa0a46e2a2f828e6eb7827707
Richard Sandberg <richard.sandberg@jsc.se>
2023-06-13 13:57:05 +0200

    New code 1

--- HttpTrigger1.cs
+++ HttpTrigger1.cs
@@ -7,22 +7,26 @@ namespace Company.Function
 {
     public class HttpTrigger1
     {
-        private readonly ILogger _logger;
-
-        public HttpTrigger1(ILoggerFactory loggerFactory)
-        {
-            _logger = loggerFactory.CreateLogger<HttpTrigger1>();
-        }
-
         [Function("HttpTrigger1")]
-        public HttpResponseData Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequestData req)
+        public static HttpResponseData Run(
+            [HttpTrigger(AuthorizationLevel.Function, "post")] HttpRequestData req,
+            FunctionContext executionContext)
         {
-            _logger.LogInformation("C# HTTP trigger function processed a request.");
+            var logger = executionContext.GetLogger("HttpTrigger1");
+            logger.LogInformation("C# HTTP trigger function processed a request.");
 
+            // Läs inkommande data från request body
+            string requestBody = req.ReadString();
+
+            // Omvandla JSON-data till array av strängar
+            string[] inputArray = Newtonsoft.Json.JsonConvert.DeserializeObject<string[]>(requestBody);
+
+            // Sortera arrayen
+            string[] sortedArray = StringArraySorter.SortStringArray(inputArray);
+
+            // Returnera den sorterade arrayen som JSON
             var response = req.CreateResponse(HttpStatusCode.OK);
-            response.Headers.Add("Content-Type", "text/plain; charset=utf-8");
-
-            response.WriteString("Welcome to Azure Functions!");
+            response.WriteString(Newtonsoft.Json.JsonConvert.SerializeObject(sortedArray));
 
             return response;
         }
